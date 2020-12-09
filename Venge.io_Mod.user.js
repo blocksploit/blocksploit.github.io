570a1d802d1c518722f8a884e7adaf7cb3296179
blocksploit <75259220+blocksploit@users.noreply.github.com>
2020-12-09 19:01:43 +0200

    Add files via upload

---
+++ Venge.io_Mod.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Venge.io Mod SECOND REWRITE [E]
+// @namespace    https://blocksploit.github.io
+// @version      0.1
+// @description  Developing...
+// @author       Blockman_#0431
+// @match        *://venge.io/*
+// @require      https://unpkg.com/guify@0.12.0/lib/guify.min.js
+// @require      http://mrdoob.github.io/stats.js/build/stats.min.js
+// @require      https://raw.githubusercontent.com/blocksploit/blocksploit/main/main.js
+// @grant        none
+// ==/UserScript==
